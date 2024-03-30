405e45b0274d893f27300ae1e5e1feb8adf51f2d
Ian Lum <ianlum314@gmail.com>
2024-03-30 10:14:03 -0400

    add damage taken and health

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 
+const MAX_HEALTH = 5
 const SPEED = 300.0
 const BULLET = {
 	SPEED = 600,
@@ -8,6 +9,22 @@ const BULLET = {
 }
 
 @onready var bullet_scene = preload("res://src/player/bullet.tscn")
+
+@onready var health = MAX_HEALTH: set = _set_health
+
+
+## --- SETTERS ---
+
+
+func _set_health(new_health):
+	health = clamp(new_health, 0, MAX_HEALTH)
+#	var hbox = $ui/ColorRect/HBoxContainer
+#	for i in hbox.get_child_count():
+#		hbox.get_child(i).visible = health > i
+
+
+## --- CORE FUNCTIONS ---
+
 
 func _physics_process(delta):
 	var direction = Vector2(
@@ -22,6 +39,9 @@ func _unhandled_input(event):
 		attack()
 
 
+## --- ACTION FUNCTIONS ---
+
+
 func attack():
 	var instance: Attack = bullet_scene.instantiate()
 	instance.start(
@@ -31,3 +51,21 @@ func attack():
 	instance.field_time = BULLET.FIELD_TIME
 	instance.speed = BULLET.SPEED
 	get_parent().add_child(instance)
+
+
+## --- TAKING DAMAGE ---
+
+
+func take_damage(damage: int):
+#	if !dash_timer.is_stopped(): return
+	health -= damage
+	if health <= 0: die()
+	
+#	Global.camera.shake(0.2, 5)
+#	var tween = create_tween()
+#	$ui/ScreenColor.color = Color(Color.RED, 0.3)
+#	tween.tween_property($ui/ScreenColor, "color", Color.TRANSPARENT, 0.3)
+
+
+func die():
+	get_tree().reload_current_scene()
