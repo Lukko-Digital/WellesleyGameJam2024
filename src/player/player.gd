fc548441ac4b9593668058f410614a4c5218f062
Ian Lum <ianlum314@gmail.com>
2024-03-30 10:53:57 -0400

    add game camera global with camera shake

--- src/player/player.gd
+++ src/player/player.gd
@@ -61,7 +61,7 @@ func take_damage(damage: int):
 	health -= damage
 	if health <= 0: die()
 	
-#	Global.camera.shake(0.2, 5)
+	Global.camera.shake(0.2, 5)
 	var tween = create_tween()
 	$ui/ScreenColor.color = Color(Color.RED, 0.3)
 	tween.tween_property($ui/ScreenColor, "color", Color.TRANSPARENT, 0.3)
