ceff5d16705c01f8740425b0eb00aaf25585ed29
Ian Lum <ianlum314@gmail.com>
2024-03-30 17:14:17 -0400

    walk animation

--- src/enemies/ranged_guy.gd
+++ src/enemies/ranged_guy.gd
@@ -1,1 +1,13 @@
 extends Enemy
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta):
+	super(delta)
+	if attacking:
+		sprite.play("idle")
+	else:
+		sprite.play("walk")
+		if velocity.x < 0:
+			sprite.flip_h = true
+		else: sprite.flip_h = false
