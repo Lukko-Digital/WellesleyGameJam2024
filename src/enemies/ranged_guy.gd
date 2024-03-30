ff985ea640b6ae34339d1b638c33650036549f2e
Ian Lum <ianlum314@gmail.com>
2024-03-30 18:36:29 -0400

    ranged guy attacks

--- src/enemies/ranged_guy.gd
+++ src/enemies/ranged_guy.gd
@@ -1,5 +1,11 @@
 extends Enemy
 
+const ATTACK = {
+	SPEED = 200,
+	FIELD_TIME = 20
+}
+
+@onready var attack_scene = preload("res://src/enemies/staff_attack.tscn")
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var staff: AnimatedSprite2D = $staff
 
@@ -15,3 +21,24 @@ func _physics_process(delta):
 		else:
 			sprite.flip_h = false
 			staff.scale = Vector2(1, 1)
+
+
+func attack():
+	staff.play("attack")
+	await staff.animation_finished
+	spawn_bullet()
+	staff.play("endlag")
+	await staff.animation_finished
+	attacking = false
+
+
+func spawn_bullet():
+	var instance: Attack = attack_scene.instantiate()
+	var dir = (player.global_position - staff.global_position).normalized()
+	instance.start(
+		position + dir * staff.offset.x + staff.position,
+		dir
+	)
+	instance.field_time = ATTACK.FIELD_TIME
+	instance.speed = ATTACK.SPEED
+	get_parent().add_child(instance)
