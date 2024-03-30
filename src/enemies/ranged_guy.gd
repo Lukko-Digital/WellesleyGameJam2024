bf8d6bf1fe5f882add1f35113577e78f4f7d296b
Ian Lum <ianlum314@gmail.com>
2024-03-30 17:23:00 -0400

    add staff

--- src/enemies/ranged_guy.gd
+++ src/enemies/ranged_guy.gd
@@ -1,6 +1,7 @@
 extends Enemy
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var staff: AnimatedSprite2D = $staff
 
 func _physics_process(delta):
 	super(delta)
@@ -10,4 +11,7 @@ func _physics_process(delta):
 		sprite.play("walk")
 		if velocity.x < 0:
 			sprite.flip_h = true
-		else: sprite.flip_h = false
+			staff.scale = Vector2(-1, 1)
+		else:
+			sprite.flip_h = false
+			staff.scale = Vector2(1, 1)
