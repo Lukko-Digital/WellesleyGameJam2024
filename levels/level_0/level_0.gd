b36c42c66a1ca439464761c09c0d1e900f2f81aa
Ian Lum <ianlum314@gmail.com>
2024-03-31 00:23:40 -0400

    spawn waves on a timer

--- levels/level_0/level_0.gd
+++ levels/level_0/level_0.gd
@@ -1,11 +1,31 @@
 extends Node2D
 
+@onready var ranged_guy_scene = preload("res://src/enemies/ranged_guy.tscn")
+@onready var enemies = $enemies
 
-# Called when the node enters the scene tree for the first time.
+var WAVES_TO_KILL = 3
+
+var SPAWN_POINTS = [
+	Vector2(-222, -73),
+	Vector2(-222, 100),
+	Vector2(222, -73)
+]
+
+var waves_spawned = 0
+
+
 func _ready():
-	pass # Replace with function body.
+	spawn_wave()
+	await get_tree().create_timer(15).timeout
+	spawn_wave()
+	await get_tree().create_timer(15).timeout
+	spawn_wave()
+	await get_tree().create_timer(15).timeout
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func spawn_wave():
+	waves_spawned += 1
+	for pos in SPAWN_POINTS:
+		var instance = ranged_guy_scene.instantiate()
+		instance.position = pos
+		enemies.add_child(instance)
