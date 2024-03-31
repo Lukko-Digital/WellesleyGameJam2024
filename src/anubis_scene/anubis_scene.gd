2e395aec0b4126bc6ad7f3afdc4589ded4fcce32
Ian Lum <ianlum314@gmail.com>
2024-03-31 10:03:20 -0400

    anubis scene 1

--- src/anubis_scene/anubis_scene.gd
+++ src/anubis_scene/anubis_scene.gd
@@ -9,26 +9,44 @@ var last_choice = -1
 
 signal CONTINUE
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
-	player_dialogue.hide()
-	character_dialogue.hide()
-	pass
+	player_dialogue.show()
+	character_dialogue.show()
+	anubis_line1()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if earth_door.EARTHDOOR:
-		character_dialogue.display_character_dialogue('EarthDoor', 0)
+	pass
+#	if earth_door.EARTHDOOR:
+#		character_dialogue.display_character_dialogue('EarthDoor', 0)
 
 func anubis_line1():
 	character_dialogue.display_character_dialogue('Anubis', 0)
 	await character_dialogue.TALKING
 	player_dialogue.display_choices_dialogue('AnubisResponse', 0)
 	await player_dialogue.SELECTED
-	if last_choice == 2:
-		character_dialogue.display_character_dialogue('Anubis', 1)
-		await character_dialogue.TALKING
-		await CONTINUE
-		anubis_line1()
+	character_dialogue.display_character_dialogue('IntoTheDetails', 0)
+	await character_dialogue.TALKING
+	character_dialogue.display_character_dialogue('IntoTheDetails', 1)
+	await character_dialogue.TALKING
+	player_dialogue.display_choices_dialogue('NotSurprised', 0)
+	await player_dialogue.SELECTED
+	character_dialogue.display_character_dialogue('Book', 0)
+	await character_dialogue.TALKING
+	player_dialogue.display_choices_dialogue('WhatBook', 0)
+	await player_dialogue.SELECTED
+	character_dialogue.display_character_dialogue('BookExplanation', 0)
+	await character_dialogue.TALKING
+	player_dialogue.display_choices_dialogue('Creepy', 0)
+	await player_dialogue.SELECTED
+	character_dialogue.display_character_dialogue('Look', 0)
+	await character_dialogue.TALKING
+#	if last_choice == 2:
+#		character_dialogue.display_character_dialogue('Anubis', 1)
+#		await character_dialogue.TALKING
+#		await CONTINUE
+#		anubis_line1()
 
 
 func _on_player_dialogue_selected(choice):
